ac941404276797f40ca79550f2807c36c45e9b95
James Strassburg <jstrassburg@gmail.com>
2012-10-21 00:51:59 -0500

    A tube picker with wired button.

--- gen/com/example/R.java
+++ gen/com/example/R.java
@@ -8,24 +8,22 @@
 package com.example;
 
 public final class R {
+    public static final class array {
+        public static final int tubes=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int EL84=0x7f050002;
-        public static final int SixLSix=0x7f050001;
-        public static final int pickTube=0x7f050003;
-        public static final int radioTube=0x7f050000;
+        public static final int selectTubeButton=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int radioEL84=0x7f040002;
-        public static final int radioSixLSixGC=0x7f040001;
-        public static final int showTube=0x7f040003;
+        public static final int selectTube=0x7f040001;
     }
 }
--- src/com/example/BiasCalculator.java
+++ src/com/example/BiasCalculator.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,23 +14,37 @@ import android.widget.Toast;
 import java.util.TooManyListenersException;
 
 public class BiasCalculator extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+    final Context context = this;
+    int whichTube = -1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Button pickTube = (Button)findViewById(R.id.pickTube);
-        pickTube.setOnClickListener(new View.OnClickListener() {
+        Button selectTubeButton = (Button)findViewById(R.id.selectTubeButton);
+        selectTubeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                RadioGroup tubeGroup = (RadioGroup)findViewById(R.id.radioTube);
-                int selectedId = tubeGroup.getCheckedRadioButtonId();
-                RadioButton tubeSelection = (RadioButton)findViewById(selectedId);
-                Toast.makeText(BiasCalculator.this, tubeSelection.getText(), Toast.LENGTH_SHORT).show();
+                selectTube();
             }
         });
     }
+
+    private void selectTube() {
+        AlertDialog.Builder tubeSelector = new AlertDialog.Builder(context);
+        tubeSelector.setTitle(R.string.selectTube);
+        tubeSelector.setItems(R.array.tubes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                whichTube = i;
+                Toast.makeText(context, Integer.toString(whichTube), Toast.LENGTH_SHORT).show();
+            }
+        });
+        tubeSelector.setCancelable(false);
+        tubeSelector.create();
+        tubeSelector.show();
+    }
+
+
 }
