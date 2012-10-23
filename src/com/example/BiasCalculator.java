657cef5f6e7408ef1082b26d8d174fafe36b9c32
James Strassburg <jstrassburg@gmail.com>
2012-10-23 15:30:58 -0500

    More official view, data validation on the number fields, some view utility functions, and a map initialized tube spinner which allows easy lookup of maximum plate dissipation for the selected tube.

--- src/com/example/BiasCalculator.java
+++ src/com/example/BiasCalculator.java
@@ -6,45 +6,63 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
+import android.widget.*;
 
-import java.util.TooManyListenersException;
+import java.util.*;
 
 public class BiasCalculator extends Activity {
     final Context context = this;
-    int whichTube = -1;
+
+    private Map<String, Float> _tubeMap;
+
+    public BiasCalculator() {
+        _tubeMap = new HashMap<String, Float>();
+        _tubeMap.put("6L6GC", 30.0f);
+        _tubeMap.put("EL84", 12.0f);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Button selectTubeButton = (Button)findViewById(R.id.selectTubeButton);
-        selectTubeButton.setOnClickListener(new View.OnClickListener() {
+        ConfigureTubeSelector();
+        ConfigureCalculateButton();
+    }
+
+    private void ConfigureCalculateButton() {
+        Button calculateButton = (Button) findViewById(R.id.calculateButton);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                selectTube();
+                Float maxPowerDissipation = getSelectedTubePowerDissipation();
+                int numberOfTubes = getNumberOfTubes();
             }
         });
     }
 
-    private void selectTube() {
-        AlertDialog.Builder tubeSelector = new AlertDialog.Builder(context);
-        tubeSelector.setTitle(R.string.selectTube);
-        tubeSelector.setItems(R.array.tubes, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                whichTube = i;
-                Toast.makeText(context, Integer.toString(whichTube), Toast.LENGTH_SHORT).show();
-            }
-        });
-        tubeSelector.setCancelable(false);
-        tubeSelector.create();
-        tubeSelector.show();
+    private int getNumberOfTubes() {
+        Spinner numberOfTubesSelector = (Spinner)findViewById(R.id.numberOfTubesSelector);
+        return Integer.getInteger((String)numberOfTubesSelector.getSelectedItem());
     }
 
+    private Float getSelectedTubePowerDissipation(){
+        Spinner tubeSelector = (Spinner)findViewById(R.id.tubeSelector);
+        String tube = (String)tubeSelector.getSelectedItem();
+        return _tubeMap.get(tube);
+    }
 
+    private Float getEditBoxFloat(int id){
+        EditText editText = (EditText)findViewById(id);
+        return Float.parseFloat(editText.getText().toString());
+    }
+
+    private void ConfigureTubeSelector() {
+        Spinner tubeSelector = (Spinner)findViewById(R.id.tubeSelector);
+        List<String> tubes = new ArrayList<String>(_tubeMap.keySet());
+        ArrayAdapter<String> tubeAdapter = new ArrayAdapter<String>(
+            context, android.R.layout.simple_spinner_dropdown_item, tubes);
+        tubeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tubeSelector.setAdapter(tubeAdapter);
+    }
 }
